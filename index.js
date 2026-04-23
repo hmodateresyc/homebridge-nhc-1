65f414dff8142f0186a54821a3327630b43c25ab
hmodateresyc <36571234+hmodateresyc@users.noreply.github.com>
2026-04-23 16:02:01 +0200

    Use modern Homebridge dynamic platform API and improve config schema UI
    - index.js: switch to modern api.registerPlatform() style (removes legacy static platform registration)
    - config.schema.json: add headerDisplay and footerDisplay for Homebridge UI
    - Bump version to 1.6.16

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 const NikoHomeControlPlatform = require('./lib/NikoHomeControlPlatform')
 
-module.exports = function (homebridge) {
-  homebridge.registerPlatform('homebridge-nhc-1', 'NikoHomeControl', NikoHomeControlPlatform, true)
+module.exports = (api) => {
+  api.registerPlatform('NikoHomeControl', NikoHomeControlPlatform)
 }
